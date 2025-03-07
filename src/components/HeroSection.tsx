0e9032ed92e8f4b456a73cdc352a63affc77629d
Akkenapally Ruthwik Reddy <131450727+ruthwwikreddy@users.noreply.github.com>
2025-03-07 17:13:02 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
           loop
           muted
           playsInline
-          poster="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thebillionaireconcierge.com%2Fluxury-concierge%2F&psig=AOvVaw0I4YSQ8CchHr_cyZrEFU0b&ust=1741434112733000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJi2iODx94sDFQAAAAAdAAAAABAE"
+          poster="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/7b/c8/bd/full-fleet-of-luxury.jpg?w=1200&h=-1&s=1"
         >
           <source
             src="https://player.vimeo.com/external/384761655.sd.mp4?s=383ab4dbc773cd0d5ece3af208d8f963368f67e4&profile_id=164&oauth2_token_id=57447761"
